7728bd56b87bf38af1a626b0f47e2b4ff3dff9a4
unknown <bmityashina@gmail.com>
2010-08-25 16:08:14 +0400

    Revision 1 - the Hello World functionality implemented.

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Varvara.Zaikina
+ * Date: Aug 25, 2010
+ * Time: 1:10:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+            System.out.print("Hello world!");
+        }
+    
+}
