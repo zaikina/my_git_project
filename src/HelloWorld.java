e563823ebe99acf555c8e2ff9c51b5f708dc3c62
unknown <bmityashina@gmail.com>
2010-09-20 12:28:09 +0400

    Commit to check pull for conflicts

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Varvara.Zaikina
@@ -6,8 +10,13 @@
  * To change this template use File | Settings | File Templates.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
-            System.out.print("Hello world!");
-        }
+    public static void main(String[] argv) throws IOException {
+                BufferedReader buff = new BufferedReader(
+                new InputStreamReader(System.in));
+                System.out.print("What's your name? ");
+                System.out.flush();
+                String s = buff.readLine();
+                System.out.printf("Hello, %s!", s);
+            }
     
 }
